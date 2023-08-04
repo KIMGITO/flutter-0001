f31cc8121711c63d818586601576493b1529b00a
KimGitonga <gitonga.kimanthi@s.karu.ac.ke>
2023-08-04 23:19:04 +0300

    enable the stateful widget of currency converter app. solve otherissues too

--- currency/lib/main.dart
+++ currency/lib/main.dart
@@ -1,25 +1,126 @@
-import 'package:currency/interface.dart';
+
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(CurrencyApp());
+void main(){
+  runApp(const Currency());
+}
 
-class CurrencyApp extends StatelessWidget {
-  const CurrencyApp({super.key});
+class Currency extends StatefulWidget{
+  const Currency({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _Currency();
+  }
+}
+
+class _Currency extends State <Currency>{
+  String amount = "0";
+  final TextEditingController textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return (MaterialApp(
+    return(MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
         appBar: AppBar(
-          toolbarHeight: 100,
-          backgroundColor: const Color.fromARGB(255, 27, 6, 219),
-          title: const Center(
-            child: Text("Currency Converter "),
-          ),
+          title:const Text("Currency Converter"),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
         ),
-        body: const Interface(),
+        body: Center(
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FractionallySizedBox(
+                  widthFactor: .8,
+                  child: Column(
+                    children: [
+                      Card(
+                      color:const Color.fromARGB(255, 255, 255, 255),
+                      child: Container(
+                        padding: const EdgeInsets.all(20),
+                        child:  Center(
+                          child: Text("$amount     USD",
+                          style:const  TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500
+                          )
+                          ),
+                        ),
+                      ),
+                    ),
+                     Padding(
+                      padding:const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                      child: TextField(
+                        controller: textEditingController,
+                        onSubmitted: (String value){
+                          setState(() {
+                            amount = (double.parse(value)/143).toStringAsFixed(4);
+                          });
+                        },
+                        style: const TextStyle(
+                          color: Color.fromARGB(255, 53, 13, 197)
+                          
+                        ),
+                        decoration:const InputDecoration(
+                          
+                          contentPadding: EdgeInsets.fromLTRB(15, 15, 8, 10),
+                          isDense: true,
+                          hintText: "Enter Amount in kesh",
+                          
+                          hintStyle: TextStyle(
+                            color: Color.fromARGB(255, 98, 106, 107),
+                          ),
+                          suffixText: "Ksh",
+                          filled: true,
+                          fillColor: Colors.white,
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color.fromARGB(255, 255, 255, 255)
+                            )
+                          ),
+                        ),
+                        
+                      ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              
+                              setState(() {
+                                amount =
+                                  (double.parse(textEditingController.text) /
+                                          143)
+                                      .toStringAsFixed(4);
+                              });
+                              
+                            },
+                            child: Row(
+                              children: [
+                                Container(
+                                  padding:const EdgeInsets.fromLTRB(4, 0, 9, 0),
+                                  child:const Text("Convert"),
+                                ),
+                                const Icon(Icons.currency_exchange)
+                              ],
+                            ),
+                            )
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+
       ),
     ));
   }
+
+  
 }
